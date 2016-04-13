27b16661a36384fb65402246395d1a9eca223d58
Jens Rasmussen <stamppot@gmail.com>
2016-04-13 18:09:59 +0200

    Comment out deploy hook

--- before_migrate.rb
+++ before_migrate.rb
@@ -1,2 +1,2 @@
-run "ln -nfs #{shared_path}/config/sphinx #{release_path}/config/sphinx"
-run "ln -nfs #{shared_path}/config/sphinx.yml #{release_path}/config/sphinx.yml"
+# run "ln -nfs #{shared_path}/config/sphinx #{release_path}/config/sphinx"
+# run "ln -nfs #{shared_path}/config/sphinx.yml #{release_path}/config/sphinx.yml"
