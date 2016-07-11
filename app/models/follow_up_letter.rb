8a80d036ab0209614b2ba5b4dd926c9e7693c19d
Jens Rasmussen <stamppot@gmail.com>
2016-07-11 19:05:42 +0200

    Different types of letters

---
+++ app/models/follow_up_letter.rb
@@ -0,0 +1,32 @@
+class FollowUpLetter < Letter
+
+  validates_uniqueness_of :surveytype, :scope => [:group_id, :follow_up], :message => "Der findes allerede et brev for denne skematype og opfølning for gruppen. Har du valgt den rigtige gruppe?"
+
+  attr_accessible :surveytype, :follow_up
+
+  def insert_text_variables(journal_entry)
+    self.letter.gsub!('{{login}}', journal_entry.login_user.login)
+    self.letter.gsub!('{{brugernavn}}', journal_entry.login_user.login)
+    self.letter.gsub!('{{password}}', journal_entry.password)
+    self.letter.gsub!('{{kodeord}}', journal_entry.password)
+    self.letter.gsub!('{{name}}', journal_entry.journal.title)
+    self.letter.gsub!('{{navn}}', journal_entry.journal.title)
+    self.letter.gsub!('{{firstname}}', journal_entry.journal.firstname)
+    self.letter.gsub!('{{fornavn}}', journal_entry.journal.firstname)
+    self.letter.gsub!('{{mor_navn}}', journal_entry.journal.parent_name || "")
+    self.letter.gsub!('{{projektnr}}', journal_entry.journal.alt_id || "")
+  end
+  
+  def to_mail_merge
+    self.letter.gsub!('{{login}}', '{ MERGEFIELD login }')
+    self.letter.gsub!('{{brugernavn}}', '{ MERGEFIELD brugernavn }')
+    self.letter.gsub!('{{password}}', '{ MERGEFIELD password }')
+    self.letter.gsub!('{{kodeord}}', '{ MERGEFIELD kodeord }')
+    self.letter.gsub!('{{name}}', '{ MERGEFIELD name }')
+    self.letter.gsub!('{{navn}}', '{ MERGEFIELD navn }')
+    self.letter.gsub!('{{firstname}}', '{ MERGEFIELD firstname }')
+    self.letter.gsub!('{{fornavn}}', '{ MERGEFIELD fornavn }')
+    self.letter.gsub!('{{mor_navn}}', '{ MERGEFIELD mor_navn }')
+    self.letter.gsub!('{{projektnr}}', '{ MERGEFIELD projektnr }')
+  end
+end
