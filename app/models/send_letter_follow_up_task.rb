2b0f0c58aa23e23c23514fcf23c9d1b030a9cf86
Jens Rasmussen <stamppot@gmail.com>
2016-09-18 20:24:13 +0200

    Complete mail task

--- app/models/send_letter_follow_up_task.rb
+++ app/models/send_letter_follow_up_task.rb
@@ -18,6 +18,7 @@ class SendLetterFollowUpTask < Task
 		self.letter.insert_text_variables(self.journal)
 
 		settings = CenterSetting.find_by_center_id_and_name(self.journal.center_id, "follow_up_mail-from")
+		puts "Could not find CenterSetting 'follow_up_mail'. You need to set mail from address" if settings.nil?
 		from = settings && settings.value || "tina.ravn@rsyd.dk"
 
 		puts "Sending letter to #{self.email} from #{from}"
