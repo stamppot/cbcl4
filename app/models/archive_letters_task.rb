4ef6a22513d38a7489f7385335cbe25327508d9f
Jens Rasmussen <stamppot@gmail.com>
2016-09-19 20:45:50 +0200

    fix letters

--- app/models/archive_letters_task.rb
+++ app/models/archive_letters_task.rb
@@ -6,8 +6,11 @@ class ArchiveLettersTask < Task
 	attr_accessible :group_id
 
 	def run
-	# TODO: find letters more than 1 month (read from CenterSetting) old which are completed. Set these to archived
-	
+		self.archived!
+		self.save
+	end
+
+	def self.run_tasks
 		settings = CenterSetting.find_by_center_id_and_name(self.journal.center_id, "archive_letters_older_than_days")
 		puts "Could not find CenterSetting 'archive_letters_older_than_days'." if settings.nil?
 		days = (settings && settings.value || "30").to_i
@@ -17,13 +20,7 @@ class ArchiveLettersTask < Task
 		TaskLog.create :name => 'ArchiveLettersTask', 
 			:message => "Archived letters #{DateTime.now.to_s(:long)}", 
 			:task_id => self.id
-		
-		self.archived!
-		self.save
-	end
 
-	def self.run_tasks
-		puts "Running all SendLetterFollowUp tasks"		
 		SendLetterFollowUpTask.where(:status => self.completed_status)
 							  .where("updated_at < ?", days.days.ago)
 							  .each {|task| task.run }
