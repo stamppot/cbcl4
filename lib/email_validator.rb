e2467145066734b9acd21263df9e94611c0487cd
Jens <stamppot@gmail.com>
2016-09-20 08:55:17 +0200

    Update email_validator.rb

--- lib/email_validator.rb
+++ lib/email_validator.rb
@@ -2,6 +2,8 @@ class EmailValidator
 
   def valid?(email)
     regex = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/
+    
+    regex.match(email)
   end
 
 end
