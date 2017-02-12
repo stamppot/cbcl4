42b871504ebae26470e7324b9ac730f3412cfdbd
Jens Rasmussen <stamppot@gmail.com>
2017-02-12 13:14:22 +0100

    info_skema WIP

---
+++ info_skema.sql
@@ -0,0 +1,52 @@
+INSERT INTO `surveys` VALUES (10,'Oplysningsskema','INFO','Oplysningsskema (Forældreskema)','1.5-16','parent','4499FF',99,'info','INFO');
+
+INSERT INTO `questions` VALUES (1500,10,1,0,1,NULL);
+INSERT INTO `question_cells` VALUES (2400,1500,'Information',1,1,NULL,NULL,'information::::Kære forældre,\n\nJeres barn er sammen med jer indkaldt til undersøgelse.\nVi vil bede jer om at udfylde oplysningsskemaet forud for jeres første besøg i ambulatoriet.',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2401,1500,'Questiontext',1,2,NULL,NULL,'questiontext::::Barnets navn:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2402,1500,'Questiontext',1,3,NULL,NULL,'questiontext::::Barnets CPR-nr.:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2403,1500,'Questiontext',1,4,NULL,NULL,'questiontext::::Skemaet er udfyldt d.:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2404,1500,'Questiontext',1,5,NULL,NULL,'questiontext::::Af::::subtext::::(navn, familieforhold til barnet)',NULL,0,NULL,NULL,NULL);
+
+INSERT INTO `questions` VALUES (1501,10,1,0,1,NULL);
+INSERT INTO `question_cells` VALUES (2405,1501,'Information',1,1,NULL,NULL,'information::::Oplysninger om mor:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2406,1501,'Questiontext',1,2,NULL,NULL,'questiontext::::Navn:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2407,1501,'Questiontext',1,3,NULL,NULL,'questiontext::::Alder:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2408,1501,'Questiontext',1,4,NULL,NULL,'questiontext::::Adresse:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2409,1501,'Questiontext',1,5,NULL,NULL,'questiontext::::Telefonnr./mobilnr:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2410,1501,'Questiontext',1,6,NULL,NULL,'questiontext::::Nuværende beskæftigelse:',NULL,0,NULL,NULL,NULL);
+
+INSERT INTO `questions` VALUES (1502,10,1,0,1,NULL);
+INSERT INTO `question_cells` VALUES (2411,1502,'Information',1,1,NULL,NULL,'information::::Oplysninger om far:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2412,1502,'Questiontext',1,2,NULL,NULL,'questiontext::::Navn:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2413,1502,'Questiontext',1,3,NULL,NULL,'questiontext::::Alder:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2414,1502,'Questiontext',1,4,NULL,NULL,'questiontext::::Adresse:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2415,1502,'Questiontext',1,5,NULL,NULL,'questiontext::::Telefonnr./mobilnr:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2416,1502,'Questiontext',1,6,NULL,NULL,'questiontext::::Nuværende beskæftigelse:',NULL,0,NULL,NULL,NULL);
+
+INSERT INTO `questions` VALUES (1503,10,1,0,1,NULL);
+INSERT INTO `question_cells` VALUES (2430,1503,'Information',1,1,NULL,NULL,'information::::Oplysninger om evt. stedfar/stedmor:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2431,1503,'Questiontext',1,2,NULL,NULL,'questiontext::::Navn:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2432,1503,'Questiontext',1,3,NULL,NULL,'questiontext::::Alder:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2433,1503,'Questiontext',1,4,NULL,NULL,'questiontext::::Adresse:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2434,1503,'Questiontext',1,5,NULL,NULL,'questiontext::::Telefonnr./mobilnr:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2435,1503,'Questiontext',1,6,NULL,NULL,'questiontext::::Nuværende beskæftigelse:',NULL,0,NULL,NULL,NULL);
+
+INSERT INTO `questions` VALUES (1504,10,1,0,1,NULL);
+INSERT INTO `question_cells` VALUES (2440,1504,'Questiontext',1,1,NULL,NULL,'questiontext::::Evt barnets/den unges eget tlf.nr.:',NULL,0,NULL,NULL,NULL);
+
+INSERT INTO `questions` VALUES (1505,10,1,0,1,NULL);
+INSERT INTO `question_cells` VALUES (2445,1505,'Information',1,1,NULL,NULL,'information::::Hvis forældrene er skilte:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2446,1505,'Questiontext',1,2,NULL,NULL,'questiontext::::Hvem har forældremyndigheden:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2447,1501,'TextBox',1,3,NULL,NULL,'questiontext::::Forhold omkring samvær',NULL,0,NULL,NULL,NULL);
+
+
+INSERT INTO `question_cells` VALUES (2450,1501,'Questiontext',1,1,NULL,NULL,'questiontext::::Barnets CPR-nr.:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2451,1501,'Questiontext',1,1,NULL,NULL,'questiontext::::Barnets CPR-nr.:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2452,1501,'Questiontext',1,1,NULL,NULL,'questiontext::::Barnets CPR-nr.:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2453,1501,'Questiontext',1,1,NULL,NULL,'questiontext::::Barnets CPR-nr.:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2454,1501,'Questiontext',1,1,NULL,NULL,'questiontext::::Barnets CPR-nr.:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2455,1501,'Questiontext',1,1,NULL,NULL,'questiontext::::Barnets CPR-nr.:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2456,1501,'Questiontext',1,1,NULL,NULL,'questiontext::::Barnets CPR-nr.:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2457,1501,'Questiontext',1,1,NULL,NULL,'questiontext::::Barnets CPR-nr.:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2458,1501,'Questiontext',1,1,NULL,NULL,'questiontext::::Barnets CPR-nr.:',NULL,0,NULL,NULL,NULL);
+INSERT INTO `question_cells` VALUES (2459,1501,'Questiontext',1,1,NULL,NULL,'questiontext::::Barnets CPR-nr.:',NULL,0,NULL,NULL,NULL);
