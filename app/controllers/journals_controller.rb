21107a7acc75497e07e166ff7111431a2277ed07
Jens Rasmussen <stamppot@gmail.com>
2018-07-11 13:27:51 +0200

    no search limit

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -249,7 +249,7 @@ class JournalsController < ApplicationController # < ActiveRbac::ComponentContro
     # phrase = phrase.split("-").reverse.join if phrase.to_i > 0
     logger.info "phrase: #{phrase}"
 
-    return if phrase.size < 5
+    # return if phrase.size < 5
     @journals = Journal.search_journals(current_user, phrase)
 
     # @journals = Journal.where("title LIKE '%#{raw_phrase}%' or cpr = '#{raw_phrase}'")
