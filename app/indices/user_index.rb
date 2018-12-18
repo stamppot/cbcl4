bb2166038bd63392cd2862292a5066bfb327bdda
Jens Rasmussen <stamppot@gmail.com>
2018-12-18 12:17:01 +0100

    users without delta

--- app/indices/user_index.rb
+++ app/indices/user_index.rb
@@ -1,10 +1,10 @@
-ThinkingSphinx::Index.define :user, :with => :active_record, :delta => true do
+ThinkingSphinx::Index.define :user, :with => :active_record do  #, :delta => true
   indexes name, :sortable => true
   indexes center.title, :as => :center_title
   indexes center.code, :as => :center_code
 
   has center_id, created_at
-  set_property :delta => true
+  # set_property :delta => true
 end
 
 
