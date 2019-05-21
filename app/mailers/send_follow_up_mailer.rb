056a888eed414954703ec82bbaaa7b9332ea556a
Jens Rasmussen <stamppot@gmail.com>
2019-05-21 21:28:24 +0200

    remove secret stuff

--- app/mailers/send_follow_up_mailer.rb
+++ app/mailers/send_follow_up_mailer.rb
@@ -6,10 +6,7 @@ class SendFollowUpMailer < ActionMailer::Base
   				:from => from,
 				:subject => "CBCL opfølgningsbrev",
 				:body => letter,
-				:content_type => "text/html",
-				:delivery_method_options => {
-       					api_key: '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'
-     				})
+				:content_type => "text/html")
   	end
 
   	def test(letter = "hello, testing", email = "stamppot@gmail.com", from = "stamppot@gmail.com")
@@ -17,9 +14,6 @@ class SendFollowUpMailer < ActionMailer::Base
   				:from => from,
 				:subject => "CBCL opfølgningsbrev test",
 				:body => letter,
-				:content_type => "text/html",
-				:delivery_method_options => {
-       					api_key: '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'
-     				})
+				:content_type => "text/html")
   	end
 end
