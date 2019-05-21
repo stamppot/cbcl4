9ddc383782f591ca88d3c8803c801bcd1e297bc9
Jens <stamppot@gmail.com>
2019-05-21 11:54:32 +0200

    add SendGrid API key
    TODO: move

--- app/mailers/send_follow_up_mailer.rb
+++ app/mailers/send_follow_up_mailer.rb
@@ -6,7 +6,10 @@ class SendFollowUpMailer < ActionMailer::Base
   				:from => from,
 				:subject => "CBCL opfølgningsbrev",
 				:body => letter,
-				:content_type => "text/html")
+				:content_type => "text/html",
+				:delivery_method_options => {
+       					api_key: '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'
+     				})
   	end
 
   	def test(letter = "hello, testing", email = "stamppot@gmail.com", from = "stamppot@gmail.com")
@@ -14,6 +17,9 @@ class SendFollowUpMailer < ActionMailer::Base
   				:from => from,
 				:subject => "CBCL opfølgningsbrev test",
 				:body => letter,
-				:content_type => "text/html")
+				:content_type => "text/html",
+				:delivery_method_options => {
+       					api_key: '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'
+     				}))
   	end
 end
