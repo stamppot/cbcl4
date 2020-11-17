a63d90968975c5fe43ac35285e05623343a1a1e8
Jens Rasmussen <stamppot@gmail.com>
2020-11-17 18:02:27 +0100

    Kæde for andre centre

--- app/models/chain_info_survey.rb
+++ app/models/chain_info_survey.rb
@@ -1,25 +1,17 @@
 class ChainInfoSurvey
 
-
-	def create_chain?(journal, survey, follow_up)
-		puts "create_chain?  follow_up: #{follow_up},  journal: #{journal.inspect}, survey: #{survey.inspect}  center: #{journal.center_id}"
-		return false unless journal.center_id == 1 || journal.center_id == 52 || journal.center_id == 9754 || journal.center_id == 9753 || journal.center_id == 9755 || journal.center_id == 8888 # BPUH, Testcenter
-		puts "center is bpuh"
-		return false unless follow_up.to_i == 0
-		return false if (survey.id == 10 || survey.id == 210) && journal.age >= 18 && journal.center_id == 1
-		
-		puts "follow_up is 0"
-		exists = journal.surveys.any? { |s| s.id == 10 || s.id == 210 }
-		puts "exists: #{exists}"
-		#return false if exists
-		#puts "surveys doesn't exist already"
-		return false unless survey.surveytype == "parent"
-		puts "surveytype is parent"
-		return true
+	def create_chain(journal, surveys, follow_up)
+		if create_chain_buph?(journal, surveys, follow_up)
+			create_chain_buph(journal, surveys, follow_up)
+		elsif create_chain_bupeav?(journal, surveys, follow_up)
+			create_chain_bupeav(journal, surveys, follow_up)
+		else
+			puts "do not create chain"
+		end
 	end
 
-	def create_chain(journal, surveys, follow_up)
-		puts "create_chain: journal: #{journal.inspect}, surveys: #{surveys.inspect}, follow_up: #{follow_up}"
+	# Forskningsenheden, Børne- og Ungdomspsykiatri Odense
+	def create_chain_buph(journal, surveys, follow_up)
 		survey = surveys.select {|s| s.surveytype == "parent"}.first
 
 		puts "found parent survey: #{survey.inspect}"
@@ -37,4 +29,63 @@ class ChainInfoSurvey
 			puts "Created chain: #{couple}, journal: #{journal.inspect}"
 		end
 	end
+
+	def create_chain_buph?(journal, survey, follow_up)
+		puts "create_chain?  follow_up: #{follow_up},  journal: #{journal.inspect}, survey: #{survey.inspect}  center: #{journal.center_id}"
+		return false unless journal.center_id == 1 || journal.center_id == 52 || journal.center_id == 9754 || journal.center_id == 9753 || journal.center_id == 9755 || journal.center_id == 8888 # BPUH, Testcenter
+		puts "center is bpuh"
+		return false unless follow_up.to_i == 0
+		return false if (survey.id == 10 || survey.id == 210) && journal.age >= 18 && journal.center_id == 1
+		
+		puts "follow_up is 0"
+		exists = journal.surveys.any? { |s| s.id == 10 || s.id == 210 }
+		puts "exists: #{exists}"
+		return false unless survey.surveytype == "parent"
+		puts "surveytype is parent"
+		return true
+	end
+
+
+	# Børne- og Ungdomspsykiatri Esbjerg, Aabenraa, Vejle
+	def create_chain_bupeav?(journal, surveys, follow_up) # Børne- og Ungdomspsykiatri Esbjerg, Aabenraa, Vejle
+		return false unless journal.center_id == 14060 || journal.center_id == 14050 || journal.center_id == 14065  # BPUH, Testcenter
+		return true
+	end
+
+	def create_chain_bupeav(journal, surveys, follow_up)
+		puts "create_chain_bupeav: journal: #{journal.inspect}, surveys: #{surveys.inspect}, follow_up: #{follow_up}"
+		
+		surveys.each do |survey|
+
+			puts "curr survey: #{survey.inspect}"
+			is_valid = survey && create_chain_bupeav?(journal, survey, follow_up)
+			puts "do create_chain #{is_valid.inspect}"
+
+			if is_valid
+				info_surveys = 
+				case survey.id
+				when 1
+				     [Survey.find(210)]   # CBCL børn forældre
+				when 2
+				 	[Survey.find(210)]   # CBCL 6-16 parent
+				when 3
+				 	[]   # C-TRF pædagog
+				when 4
+				 	[]   # TRF teacher 
+				when 5
+				  	[Survey.find(11)]   # YSR youth
+				else
+				 	[]
+				end
+				couple = info_surveys.any? && {survey.id => info_surveys.first.id} || {}
+				puts "couple: #{couple.inspect}"
+				entries = journal.add_journal_entries(info_surveys, follow_up, true)
+				puts "entries to couple: #{entries.inspect}"
+				JournalEntryService.new.connect(journal, couple, true) if couple.any?
+				# logger.info "Created chain: #{couple}, journal: #{journal.inspect}"
+				puts "Created chain: #{couple}, journal: #{journal.inspect}"
+			end
+		end
+	end
+
 end
